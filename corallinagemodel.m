3c385c2ec230e69b2369ad28d9f402c4fd139933
Christian Casadio <casadiochristian@gmail.com>
2018-05-24 23:53:56 -0500

    added power spectrum to find points per year

--- corallinagemodel.m
+++ corallinagemodel.m
@@ -1,27 +1,36 @@
 function [  ] = corallinagemodel( inputData )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-out = linearAgeModel(inputData,12,2.7);
+
+figure;
+[pow,w] = pwelch(inputData(:,2));
+plot(w,pow);
+hold on;
+[~, loc] = findpeaks(pow, w, 'SortStr', 'descend','MinPeakHeight',.1);%'NPeaks', 1,
+ppy = 2*pi/loc(1)
+
+figure;
+out = linearAgeModel(inputData,ppy,2.7);
 plot(inputData(:,1),inputData(:,2),'k*');
 hold on;
-plot(out(:,1),out(:,2),'ro');
+plot(out(:,1)+2/24,out(:,2),'ro');
 
 end
 
 function [data] = linearAgeModel(inputData, numberpointsperyear, splinesensitivity)
 
-    numbins = round(length(inputData)/numberpointsperyear);
+    numbins = round(length(inputData)/numberpointsperyear)
 
     % create new interpolated timeseries
     % Constructing a cubic spline fit to the data using numknots (# of knots)
     % = the number of bins * an arbitrary splinesensitivity input above
-    numknots = numbins*splinesensitivity;
+    numknots = numbins*splinesensitivity
 
-    for ww = 1:length(inputData(:,2))
-        if isnan(inputData(ww,2)) == 1;
-            w(ww) = 0;
+    for i = 1:length(inputData(:,2))
+        if isnan(inputData(i,2)) == 1;
+            w(i) = 0;
         else
-            w(ww) = 1;
+            w(i) = 1;
         end
     end
     w = w';
@@ -30,11 +39,11 @@ function [data] = linearAgeModel(inputData, numberpointsperyear, splinesensitivi
     % gets rid of spurious 0's due to NaN's in original data to make NaN in the
     % spline curve for plotting purposes
     splined18O = fnval(sp,inputData(:,1));
-    for  ww = 1:length(inputData(:,2))
-        if splined18O(ww) == 0;
-            splined18O(ww) = NaN;
+    for  i = 1:length(inputData(:,2))
+        if splined18O(i) == 0;
+            splined18O(i) = NaN;
         else
-            splined18O(ww) = splined18O(ww);
+            splined18O(i) = splined18O(i);
         end
     end
 
@@ -45,30 +54,28 @@ function [data] = linearAgeModel(inputData, numberpointsperyear, splinesensitivi
     % Logging the inflection points where the first derivative = 0 (sign
     % change)
     counter = 1;
-    for j = 1:length(fderval)-1
-        if fderval(j,1) > 0 && fderval(j+1,1) < 0
-            if isnan(inputData(j,2)) == 1
-                inflections(counter,:) = [inputData(j,1) splined18O(j)];
+    for i = 1:length(fderval)-1
+        if fderval(i,1) > 0 && fderval(i+1,1) < 0
+            if isnan(inputData(i,2)) == 1
+                inflections(counter,:) = [inputData(i,1) splined18O(i)];
             else
-                inflections(counter,:) = inputData(j,:);
+                inflections(counter,:) = inputData(i,:);
             end
             counter = counter + 1;
-        elseif fderval(j,1) < 0 && fderval(j+1,1) > 0
-            if isnan(inputData(j,2)) == 1
-                inflections(counter,:) = [inputData(j,1) splined18O(j)];
+        elseif fderval(i,1) < 0 && fderval(i+1,1) > 0
+            if isnan(inputData(i,2)) == 1
+                inflections(counter,:) = [inputData(i,1) splined18O(i)];
             else
-                inflections(counter,:) = inputData(j,:);
+                inflections(counter,:) = inputData(i,:);
             end
             counter = counter + 1;
-        else
-            counter = counter;
         end
     end
 
     % computing mean "distance" for a year. This number should be close to the
     % number input by the user in the beginning of the program
-    for ii = 1:length(inflections)-1
-        meanyears(ii,1) = inflections(ii+1,1)-inflections(ii,1);
+    for i = 1:length(inflections)-1
+        meanyears(i,1) = inflections(i+1,1)-inflections(i,1);
     end
     meandistance = mean(meanyears);
 
@@ -80,92 +87,88 @@ function [data] = linearAgeModel(inputData, numberpointsperyear, splinesensitivi
     % compiling all of the winters, which is the max value in the
     % surrounding five data points from the inflection point
     % If data gaps are filled using NaN, then the splined18O value is used 
-    counterer = 1;
-    for j = 2:length(fderval)-1
-        if fderval(j,1) > 0 && fderval(j+1,1) < 0
-            if isnan(inputData(j-2,2))||isnan(inputData(j-1,2))||isnan(inputData(j,2))||isnan(inputData(j+1,2))||isnan(inputData(j+2,2)) == 1;
-                if isnan(inputData(j-2,2)) == 1
-                    newminus2 = [inputData(j-2,1) splined18O(j-2,:)];
+    counter = 1;
+    for i = 2:length(fderval)-1
+        if fderval(i,1) > 0 && fderval(i+1,1) < 0
+            if isnan(inputData(i-2,2))||isnan(inputData(i-1,2))||isnan(inputData(i,2))||isnan(inputData(i+1,2))||isnan(inputData(i+2,2)) == 1;
+                if isnan(inputData(i-2,2)) == 1
+                    newminus2 = [inputData(i-2,1) splined18O(i-2,:)];
                 else
-                    newminus2 = inputData(j-2,:);
+                    newminus2 = inputData(i-2,:);
                 end
-                if isnan(inputData(j-1,2)) == 1
-                    newminus1 = [inputData(j-1,1) splined18O(j-1,:)];
+                if isnan(inputData(i-1,2)) == 1
+                    newminus1 = [inputData(i-1,1) splined18O(i-1,:)];
                 else
-                    newminus1 = inputData(j-1,:);
+                    newminus1 = inputData(i-1,:);
                 end
-                if isnan(inputData(j,2)) == 1
-                    newone = [inputData(j,1) splined18O(j,:)];
+                if isnan(inputData(i,2)) == 1
+                    newone = [inputData(i,1) splined18O(i,:)];
                 else
-                    newone = inputData(j,:);
+                    newone = inputData(i,:);
                 end
-                if isnan(inputData(j+1,2)) == 1
-                    newplus1 = [inputData(j+1,1) splined18O(j+1,:)];
+                if isnan(inputData(i+1,2)) == 1
+                    newplus1 = [inputData(i+1,1) splined18O(i+1,:)];
                 else
-                    newplus1 = inputData(j+1,:);
+                    newplus1 = inputData(i+1,:);
                 end
-                if isnan(inputData(j+1,2)) == 1
-                    newplus2 = [inputData(j+2,1) splined18O(j+2,:)];
+                if isnan(inputData(i+1,2)) == 1
+                    newplus2 = [inputData(i+2,1) splined18O(i+2,:)];
                 else
-                    newplus2 = inputData(j+2,:);
+                    newplus2 = inputData(i+2,:);
                 end
                 winterblock = [newminus2;newminus1;newone;newplus1;newplus2];
-                [r,c,v] = find(winterblock == max(winterblock(:,2)));
-                winter(counterer,:) = winterblock(r(1,1),:);
+                [r,~,~] = find(winterblock == max(winterblock(:,2)));
+                winter(counter,:) = winterblock(r(1,1),:);
             else
-                winterblock = [inputData(j-2,:);inputData(j-1,:);inputData(j,:);inputData(j+1,:);inputData(j+2,:)];
-                [r,c,v] = find(winterblock == max(winterblock(:,2)));
-                winter(counterer,:) = winterblock(r(1,1),:);
+                winterblock = [inputData(i-2,:);inputData(i-1,:);inputData(i,:);inputData(i+1,:);inputData(i+2,:)];
+                [r,~,~] = find(winterblock == max(winterblock(:,2)));
+                winter(counter,:) = winterblock(r(1,1),:);
             end
-            counterer = counterer + 1;
-        else
-            counterer = counterer;
+            counter = counter + 1;
         end
     end
 
     % compiling all of summers, which is the min value in the
     % surrounding five data points from the inflection point
     % If data gaps are filled using NaN, then the splined18O value is used
-    counterer = 1;
-    for j = 2:length(fderval)-1
-        if fderval(j,1) < 0 && fderval(j+1,1) > 0
-            if isnan(inputData(j-2,2))||isnan(inputData(j-1,2))||isnan(inputData(j,2))||isnan(inputData(j+1,2))||isnan(inputData(j+2,2)) == 1;
-                if isnan(inputData(j-1,2)) == 1
-                    newminus2 = [inputData(j-2,1) splined18O(j-2,:)];
+    counter = 1;
+    for i = 2:length(fderval)-1
+        if fderval(i,1) < 0 && fderval(i+1,1) > 0
+            if isnan(inputData(i-2,2))||isnan(inputData(i-1,2))||isnan(inputData(i,2))||isnan(inputData(i+1,2))||isnan(inputData(i+2,2)) == 1;
+                if isnan(inputData(i-1,2)) == 1
+                    newminus2 = [inputData(i-2,1) splined18O(i-2,:)];
                 else
-                    newminus2 = inputData(j-2,:);
+                    newminus2 = inputData(i-2,:);
                 end
-                if isnan(inputData(j-1,2)) == 1
-                    newminus1 = [inputData(j-1,1) splined18O(j-1,:)];
+                if isnan(inputData(i-1,2)) == 1
+                    newminus1 = [inputData(i-1,1) splined18O(i-1,:)];
                 else
-                    newminus1 = inputData(j-1,:);
+                    newminus1 = inputData(i-1,:);
                 end
-                if isnan(inputData(j,2)) == 1
-                    newone = [inputData(j,1) splined18O(j,:)];
+                if isnan(inputData(i,2)) == 1
+                    newone = [inputData(i,1) splined18O(i,:)];
                 else
-                    newone = inputData(j,:);
+                    newone = inputData(i,:);
                 end
-                if isnan(inputData(j+1,2)) == 1
-                    newplus1 = [inputData(j+1,1) splined18O(j+1,:)];
+                if isnan(inputData(i+1,2)) == 1
+                    newplus1 = [inputData(i+1,1) splined18O(i+1,:)];
                 else
-                    newplus1 = inputData(j+1,:);
+                    newplus1 = inputData(i+1,:);
                 end
-                if isnan(inputData(j+2,2)) == 1
-                    newplus2 = [inputData(j+2,1) splined18O(j+2,:)];
+                if isnan(inputData(i+2,2)) == 1
+                    newplus2 = [inputData(i+2,1) splined18O(i+2,:)];
                 else
-                    newplus2 = inputData(j+2,:);
+                    newplus2 = inputData(i+2,:);
                 end
                 summerblock = [newminus2;newminus1;newone;newplus1;newplus2];
-                [r,c,v] = find(summerblock == min(summerblock(:,2)));
-                summer(counterer,:) = summerblock(r(1,1),:);
+                [r,~,~] = find(summerblock == min(summerblock(:,2)));
+                summer(counter,:) = summerblock(r(1,1),:);
             else
-                summerblock = [inputData(j-2,:);inputData(j-1,:);inputData(j,:);inputData(j+1,:);inputData(j+2,:)];
-                [r,c,v] = find(summerblock == min(summerblock(:,2)));
-                summer(counterer,:) = summerblock(r(1,1),:);
+                summerblock = [inputData(i-2,:);inputData(i-1,:);inputData(i,:);inputData(i+1,:);inputData(i+2,:)];
+                [r,~,~] = find(summerblock == min(summerblock(:,2)));
+                summer(counter,:) = summerblock(r(1,1),:);
             end
-            counterer = counterer + 1;
-        else
-            counterer = counterer;
+            counter = counter + 1;
         end
     end
 
@@ -184,52 +187,52 @@ function [data] = linearAgeModel(inputData, numberpointsperyear, splinesensitivi
     % valleys, or summers and winters picked previously to make 5 datapoints
     % between each peak and valley to have 12 points/months in a year
 
-    counttime = 1;
+    count = 1;
     % Case 1
     if summer(1,1) < winter (1,1) && length(summer) > length(winter)
         for i = 1:(length(summer))
             if i < length(summer)
-                newdepth(counttime:counttime+5) = summer(i,1):(winter(i,1)-summer(i,1))/6:winter(i,1)-(winter(i,1)-summer(i,1))/6;
-                newdepth(counttime+6:counttime+11) = winter(i,1):(summer(i+1,1)-winter(i,1))/6:summer(i+1)-(summer(i+1,1)-winter(i,1))/6;
+                newdepth(count:count+5) = summer(i,1):(winter(i,1)-summer(i,1))/6:winter(i,1)-(winter(i,1)-summer(i,1))/6;
+                newdepth(count+6:count+11) = winter(i,1):(summer(i+1,1)-winter(i,1))/6:summer(i+1)-(summer(i+1,1)-winter(i,1))/6;
             elseif i == length(summer)
                 xxxx = length(newdepth);
                 newdepth(xxxx+1) = summer(length(summer(:,1)),1);
             end
-            counttime = counttime + 12;
+            count = count + 12;
         end
     % Case 2
     elseif summer(1,1) < winter (1,1) && length(summer) == length(winter)
         for i = 1:(length(summer))
             if i < length(summer)
-                newdepth(counttime:counttime+5) = summer(i,1):(winter(i,1)-summer(i,1))/6:winter(i,1)-(winter(i,1)-summer(i,1))/6;
-                newdepth(counttime+6:counttime+11) = winter(i,1):(summer(i+1,1)-winter(i,1))/6:summer(i+1)-(summer(i+1,1)-winter(i,1))/6;
+                newdepth(count:count+5) = summer(i,1):(winter(i,1)-summer(i,1))/6:winter(i,1)-(winter(i,1)-summer(i,1))/6;
+                newdepth(count+6:count+11) = winter(i,1):(summer(i+1,1)-winter(i,1))/6:summer(i+1)-(summer(i+1,1)-winter(i,1))/6;
             elseif i == length(summer)
-                newdepth(counttime:counttime+6) = summer(i,1):(winter(i,1)-summer(i,1))/6:winter(i,1);
+                newdepth(count:count+6) = summer(i,1):(winter(i,1)-summer(i,1))/6:winter(i,1);
             end
-            counttime = counttime + 12;
+            count = count + 12;
         end
     % Case 3
     elseif summer(1,1) > winter (1,1) && length(summer) < length(winter)
         for i = 1:(length(winter))
             if i < length(winter)
-                newdepth(counttime:counttime+5) = winter(i,1):(summer(i,1)-winter(i,1))/6:summer(i)-(summer(i,1)-winter(i,1))/6;
-                newdepth(counttime+6:counttime+11) = summer(i,1):(winter(i+1,1)-summer(i,1))/6:winter(i+1)-(winter(i+1,1)-summer(i,1))/6;
+                newdepth(count:count+5) = winter(i,1):(summer(i,1)-winter(i,1))/6:summer(i)-(summer(i,1)-winter(i,1))/6;
+                newdepth(count+6:count+11) = summer(i,1):(winter(i+1,1)-summer(i,1))/6:winter(i+1)-(winter(i+1,1)-summer(i,1))/6;
             elseif i == length(winter)
                 xxxx = length(newdepth);
                 newdepth(xxxx+1) = winter(length(winter(:,1)),1);
             end
-            counttime = counttime + 12;
+            count = count + 12;
         end
     % Case 4
     elseif summer(1,1) > winter (1,1) && length(summer) == length(winter)
         for i = 1:(length(winter))
             if i < length(winter)
-                newdepth(counttime:counttime+5) = winter(i,1):(summer(i,1)-winter(i,1))/6:summer(i)-(summer(i,1)-winter(i,1))/6;
-                newdepth(counttime+6:counttime+11) = summer(i,1):(winter(i+1,1)-summer(i,1))/6:winter(i+1)-(winter(i+1,1)-summer(i,1))/6;
+                newdepth(count:count+5) = winter(i,1):(summer(i,1)-winter(i,1))/6:summer(i)-(summer(i,1)-winter(i,1))/6;
+                newdepth(count+6:count+11) = summer(i,1):(winter(i+1,1)-summer(i,1))/6:winter(i+1)-(winter(i+1,1)-summer(i,1))/6;
             elseif i == length(winter)
-                newdepth(counttime:counttime+6) = winter(i,1):(summer(i,1)-winter(i,1))/6:summer(i);
+                newdepth(count:count+6) = winter(i,1):(summer(i,1)-winter(i,1))/6:summer(i);
             end
-            counttime = counttime + 12;
+            count = count + 12;
         end
     end
 
@@ -238,18 +241,18 @@ function [data] = linearAgeModel(inputData, numberpointsperyear, splinesensitivi
     % adding in the timestep before the first peak and after the last to
     % provide three timesteps for the seasonal averaging
     if summer(1,1) < winter(1,1)
-        [rb,cb,vb] = find(inputData(:,1) == summer(1,1));
+        [rb,~,~] = find(inputData(:,1) == summer(1,1));
         if summer(length(summer),1) > winter(length(winter),1)
-            [re,ce,ve] = find(inputData(:,1) == summer(length(summer),1));
+            [re,~,~] = find(inputData(:,1) == summer(length(summer),1));
         elseif summer(length(summer),1) < winter(length(winter),1)
-            [re,ce,ve] = find(inputData(:,1) == winter(length(winter),1));
+            [re,~,~] = find(inputData(:,1) == winter(length(winter),1));
         end
     elseif summer(1,1) > winter(1,1)
-        [rb,cb,vb] = find(inputData(:,1) == winter(1,1));
+        [rb,~,~] = find(inputData(:,1) == winter(1,1));
         if summer(length(summer),1) > winter(length(winter),1)
-            [re,ce,ve] = find(inputData(:,1) == summer(length(summer),1));
+            [re,~,~] = find(inputData(:,1) == summer(length(summer),1));
         elseif summer(length(summer),1) < winter(length(winter),1)
-            [re,ce,ve] = find(inputData(:,1) == winter(length(winter),1));
+            [re,~,~] = find(inputData(:,1) == winter(length(winter),1));
         end
     end
 
@@ -324,5 +327,5 @@ function [data] = linearAgeModel(inputData, numberpointsperyear, splinesensitivi
     % Coral Timeseries Input for Event Picker
     %======================================
 
-    data=[time/12.+(11/12) data(:,1)];
+    data=[time/12 data(:,1)];
 end
